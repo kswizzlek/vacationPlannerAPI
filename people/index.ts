ae629377d499085a8b09a18dc1de87d716a2d603
kswizzlek <kid7kruger@gmail.com>
2021-02-16 22:24:53 -0500

    added some new endpoints

---
+++ people/index.ts
@@ -0,0 +1,33 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+
+};
+
+const searchPeopleByUsername = (username: string) => {
+
+}
+
+const findPeopleOnTrip = (tripUuid: string) => {
+
+}
+
+const updateUsersPerson = (updateUsersPerson: any) => {
+
+}
+
+const addPersonToTrip = (personUuid: string, tripUuid) => {
+
+}
+
+export default httpTrigger;
