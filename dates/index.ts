622026e4c7db69749f40bcc65bc993cb8c176275
kswizzlek <kid7kruger@gmail.com>
2021-02-22 21:52:02 -0500

    added dates endpoint

---
+++ dates/index.ts
@@ -0,0 +1,42 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import addAuth from '../auth0/auth0Decorator';
+import getCosmosDbConnection from '../cosmosdb/getComsosDbConnection';
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest | any): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+
+    let resBody = {};
+
+    console.log(req)
+
+    const datesCollection = await getCosmosDbConnection("vacation", "dates");
+
+    if(req.method === "GET"){
+        resBody = await getTripDates(req.query.tripUuid, datesCollection);
+    }else if(req.method === "POST"){
+        resBody = await addDateToTrip(req.body, datesCollection);
+    }
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: resBody
+    };
+};
+
+const getTripDates = async (tripUuid: string, datesCollection: any) => {
+    return await datesCollection.findOne({tripUuid: tripUuid});
+}
+
+const addDateToTrip = async (reqBody: any, datesCollection: any) => {
+    const tripDates = {
+        tripUuid: reqBody.tripUuid,
+        dates: reqBody.tripDates
+
+    }
+
+    await datesCollection.update({tripUuid: reqBody.tripUuid}, tripDates, {upsert: true});
+
+    return tripDates;
+}
+
+export default httpTrigger;
