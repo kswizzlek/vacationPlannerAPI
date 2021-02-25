fd53c5f81b8fbda0cc4c94753a744e40d5f2f4c3
kswizzlek <kid7kruger@gmail.com>
2021-02-24 22:30:47 -0500

    added proper groupname

--- dates/index.ts
+++ dates/index.ts
@@ -16,7 +16,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     }else if(req.method === "POST"){
         resBody = await addDateToTrip(req.body, datesCollection);
         const updates = [{
-            groupname: req.query.tripUuid,
+            groupName: req.query.tripUuid,
             target: 'dates',
             arguments: [resBody]
         }];
